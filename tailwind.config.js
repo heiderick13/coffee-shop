843c8e6b08379b63c77b2f3ee98f25c1f7e75c06
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-27 09:22:02 -0300

    extend color & font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkCyan: "#0E8784",
+        "dark0grey-blue": "#333D4B",
+        "pale-orange": "#FDD6BA",
+        grey: "#83888F",
+        "light-cream": "#FEFCF7",
+      },
+      fontFamily: {
+        fraunces: ["Fraunces", "serif"],
+        barlow: ["Barlow", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
