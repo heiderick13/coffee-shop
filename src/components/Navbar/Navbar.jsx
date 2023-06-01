512f8bc510c4c7f367c569979cfde6b390dacd44
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-01 15:14:50 -0300

    :iphone: make navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import logo from "../../assets/logo.svg";
 
+import { MdMenu } from "react-icons/md";
+
 function Navbar() {
+  const openMenu = () => {
+    // function from props
+  };
   return (
-    <nav className="flex justify-between p-4">
+    <nav className="flex items-center justify-between p-4">
       <img src={logo} alt="Company Logo" />
-      <div className="flex gap-4 uppercase">
+      <div className="md:hidden text-3xl cursor-pointer" onClick={openMenu}>
+        <MdMenu />
+      </div>
+      <div className="hidden md:flex gap-4 uppercase">
         <a className="">home</a>
         <a className="">about us</a>
         <a className="">create your plan</a>
