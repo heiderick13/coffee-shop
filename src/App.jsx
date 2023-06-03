c0a93456ac02fb882e4680fe9259c6d579a8d87a
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-03 14:41:24 -0300

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="p-3">
+      <Navbar />
+      <div className="home-hero fraunces text-center rounded-4">
+        <h1>Great coffee made simple</h1>
+        <p className="barlow">
+          Start your mornings with the world’s best coffees. Try our expertly
+          curated artisan coffees from our best roasters delivered directly to
+          your door, at your schedule.
+        </p>
+        <button className="btn">Create you plan</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
