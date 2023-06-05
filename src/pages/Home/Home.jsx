ff3ce84190bd17ad16acfcc75ef509b7d6b4e0c7
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-05 15:35:15 -0300

    pages

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home-hero d-flex fraunces text-center rounded-4 px-5">
+      <h1 className="mb-5">Great coffee made simple</h1>
+      <p className="barlow mb-5">
+        Start your mornings with the world’s best coffees. Try our expertly
+        curated artisan coffees from our best roasters delivered directly to
+        your door, at your schedule.
+      </p>
+      <button className="btn main-btn rounded-3">Create you plan</button>
+    </div>
+  );
+}
+
+export default Home;
