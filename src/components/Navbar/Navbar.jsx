5585fea406f234e018343594958469b22c0ee097
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-06 16:37:34 -0300

    set Link routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import logo from "../../assets/logo.svg";
 
 import { MdMenu } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const openMenu = () => {
@@ -10,14 +11,22 @@ function Navbar() {
   };
   return (
     <nav className="navbar d-flex mb-5">
-      <img src={logo} alt="Company Logo" />
+      <Link>
+        <img src={logo} alt="Company Logo" />
+      </Link>
       <div className="nav-menu" onClick={openMenu}>
         <MdMenu />
       </div>
       <div className="nav-links">
-        <a className="">home</a>
-        <a className="">about us</a>
-        <a className="">create your plan</a>
+        <Link to={"/"} className="">
+          home
+        </Link>
+        <Link to={"/about-us"} className="">
+          about us
+        </Link>
+        <Link to={"/subscribe"} className="">
+          create your plan
+        </Link>
       </div>
     </nav>
   );
