3bfb04b3d024d3179831da9e4fe842654b6810f9
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-08 17:40:51 -0300

    style & toggle mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,49 @@
 import "./Navbar.css";
 
+import { useState } from "react";
+
 import logo from "../../assets/logo.svg";
 
-import { MdMenu } from "react-icons/md";
+import { MdMenu, MdClear } from "react-icons/md";
 import { Link } from "react-router-dom";
 
+import MobileMenu from "../MobileMenu/MobileMenu";
+
 function Navbar() {
-  const openMenu = () => {
-    // function from props
+  const [isClosed, setIsClosed] = useState(false);
+  const toggleMenu = () => {
+    setIsClosed(!isClosed);
+    console.log(isClosed);
   };
   return (
-    <nav className="navbar d-flex mb-5">
-      <Link>
-        <img src={logo} alt="Company Logo" />
-      </Link>
-      <div className="nav-menu" onClick={openMenu}>
-        <MdMenu />
-      </div>
-      <div className="nav-links">
-        <Link to={"/"} className="">
-          home
+    <>
+      <nav className="navbar d-flex mb-5">
+        <Link>
+          <img src={logo} alt="Company Logo" />
         </Link>
-        <Link to={"/about-us"} className="">
-          about us
-        </Link>
-        <Link to={"/subscribe"} className="">
-          create your plan
-        </Link>
-      </div>
-    </nav>
+        {isClosed ? (
+          <div className="nav-menu" onClick={toggleMenu}>
+            <MdMenu />
+          </div>
+        ) : (
+          <div className="nav-menu z-100" onClick={toggleMenu}>
+            <MdClear />
+          </div>
+        )}
+        <div className="nav-links">
+          <Link to={"/"} className="">
+            home
+          </Link>
+          <Link to={"/about-us"} className="">
+            about us
+          </Link>
+          <Link to={"/subscribe"} className="">
+            create your plan
+          </Link>
+        </div>
+      </nav>
+      {!isClosed && <MobileMenu toggleMenu={toggleMenu} />}
+    </>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="home-hero d-flex fraunces text-center rounded-4 px-5">
-      <h1 className="mb-5">Great coffee made simple</h1>
-      <p className="barlow mb-5">
-        Start your mornings with the world’s best coffees. Try our expertly
-        curated artisan coffees from our best roasters delivered directly to
-        your door, at your schedule.
-      </p>
-      <Link to={"/subscribe"} className="btn main-btn rounded-3">
-        Create you plan
-      </Link>
+    <div className="home">
+      <div className="home-hero d-flex fraunces text-center rounded-4 px-5">
+        <h1 className="mb-5">Great coffee made simple</h1>
+        <p className="barlow mb-5">
+          Start your mornings with the world’s best coffees. Try our expertly
+          curated artisan coffees from our best roasters delivered directly to
+          your door, at your schedule.
+        </p>
+        <Link to={"/subscribe"} className="btn main-btn rounded-3">
+          Create you plan
+        </Link>
+      </div>
     </div>
   );
 }
