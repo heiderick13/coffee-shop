fbc329b2979f5f2394a51c0166fa1ad1b89f37c0
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-12 20:00:41 -0300

    :lipstick: importe card into home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,11 @@ import espresso from "../../assets/home/desktop/image-gran-espresso.png";
 import planalto from "../../assets/home/desktop/image-planalto.png";
 import piccollo from "../../assets/home/desktop/image-piccollo.png";
 import danche from "../../assets/home/desktop/image-danche.png";
+import ProsCard from "../../components/ProsCard/ProsCard";
+
+import coffeeBean from "../../assets/home/desktop/icon-coffee-bean.svg";
+import gift from "../../assets/home/desktop/icon-gift.svg";
+import truck from "../../assets/home/desktop/icon-truck.svg";
 
 function Home() {
   return (
@@ -51,6 +56,23 @@ function Home() {
           description={
             "Ethiopian hand-harvested blend densely packed with vibrant fruit notes"
           }
+        />
+      </div>
+      <div className="pros d-flex">
+        <ProsCard
+          icon={coffeeBean}
+          pro={`Best quality`}
+          description={`Discover an endless variety of the world’s best artisan coffee from each of our roasters.`}
+        />
+        <ProsCard
+          icon={gift}
+          pro={`Exclusive benefits`}
+          description={`Special offers and swag when you subscribe, including 30% off your first shipment.`}
+        />
+        <ProsCard
+          icon={truck}
+          pro={`Free shipping`}
+          description={`We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.`}
         />
       </div>
     </div>
