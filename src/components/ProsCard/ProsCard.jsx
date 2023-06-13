a1dbca0fe0d773cf13b54fe50d6a2263988505dd
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-13 15:14:59 -0300

    :lipstick: update pros card component

--- src/components/ProsCard/ProsCard.jsx
+++ src/components/ProsCard/ProsCard.jsx
@@ -4,7 +4,7 @@ function ProsCard({ icon, pro, description }) {
   return (
     <div className="pro-card rounded-4 text-center">
       <img src={icon} alt={(icon = "icon")} />
-      <h2 className="fraunces">{pro}</h2>
+      <h3 className="fraunces">{pro}</h3>
       <p className="barlow">{description}</p>
     </div>
   );
