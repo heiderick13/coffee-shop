d305e5de6d56995b3c067b74857c3aa5da9591f2
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-13 22:37:44 -0300

    :sparkles: import footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 import { Outlet } from "react-router";
 
 function App() {
   return (
-    <div className="p-4">
+    <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
