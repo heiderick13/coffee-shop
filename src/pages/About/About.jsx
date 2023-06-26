20a924aa7de527bdf000e2c93fd404b28c211a4c
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-06-26 19:46:38 -0300

    add commitment section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Hero from "../../components/Hero/Hero";
 
-// import background from "../../assets/about/desktop/image-hero-whitecup.jpg";
-// import barista from "../../assets/about/";
+import commitmentImg from "../../assets/about/desktop/image-commitment.jpg";
 
 function About() {
   return (
@@ -20,7 +18,31 @@ function About() {
           shipment.
         </p>
       </div>
-      <div className="flex flex-col md:flex-row"></div>
+      <div className="flex flex-col md:flex-row md:items-center md:justify-center gap-8 mt-32 md:mt-16">
+        <img
+          className="rounded-xl max-h-[40rem]"
+          src={commitmentImg}
+          alt="Our Barista doing latte art"
+        />
+        <div className="px-8 text-center md:text-left md:max-w-3xl">
+          <h3 className="font-fraunces text-[2.4rem] text-dark-grey-blue">
+            Our commitment
+          </h3>
+          <p className="font-barlow text-[1.5rem]">
+            We’re built on a simple mission and a commitment to doing good along
+            the way. We want to make it easy for you to discover and brew the
+            world’s best coffee at home. It all starts at the source. To locate
+            the specific lots we want to purchase, we travel nearly 60 days a
+            year trying to understand the challenges and opportunities in each
+            of these places. We collaborate with exceptional coffee growers and
+            empower a global community of farmers through with well above
+            fair-trade benchmarks. We also offer training, support farm
+            community initiatives, and invest in coffee plant science. Curating
+            only the finest blends, we roast each lot to highlight tasting
+            profiles distinctive to their native growing region.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
